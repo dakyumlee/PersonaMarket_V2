b1ee83866f8cedaf44c6bcae37327b07cf883b83
“dakyumlee” <oicrcutie@gmail.com>
2025-11-25 01:28:32 +0900

    변수명틀림

--- backend/src/main/java/com/personamarket/entity/Persona.java
+++ backend/src/main/java/com/personamarket/entity/Persona.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
@@ -81,8 +82,7 @@ public class Persona {
     @Column(length = 100)
     private String finetuneModelId;
 
-    @Column
-    private Double price;
+    private BigDecimal price;
 
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
